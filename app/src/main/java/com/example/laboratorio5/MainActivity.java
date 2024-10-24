6a959c82990fbf3584ae78cb90fa2ffdf6922ad2
HeidiCordova <hcordovas@unsa.edu.pe>
2024-10-23 22:56:14 -0500

    Actividad 2 realizada, los botones dirigen a los fragments respectivamente

--- app/src/main/java/com/example/laboratorio5/MainActivity.java
+++ app/src/main/java/com/example/laboratorio5/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.laboratorio5;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,8 +23,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
+        LinearLayout button1 = findViewById(R.id.custom_button1);
+        LinearLayout button2 = findViewById(R.id.custom_button2);
+
+        ImageView image1 = button1.findViewById(R.id.button_image);
+        TextView text1 = button1.findViewById(R.id.button_text);
+
+        ImageView image2 = button2.findViewById(R.id.button_image);
+        TextView text2 = button2.findViewById(R.id.button_text);
+
+// Configurar imagen y texto del primer botón
+        image1.setImageResource(R.drawable.catedral); // Imagen de la Catedral
+        text1.setText("Catedral de Arequipa");
+
+// Configurar imagen y texto del segundo botón
+        image2.setImageResource(R.drawable.claustro); // Imagen del Claustro
+        text2.setText("Claustros de la Compañía");
+
 
         button1.setOnClickListener(v -> {
             Fragment fragment = new Fragment1();
